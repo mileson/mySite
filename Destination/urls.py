0e47235f6777fab4dba46c606b71dde35970834e
libingfeng <libingfeng007@gmail.com>
2018-06-05 00:22:45 +0800

    初始化

--- Destination/urls.py
+++ Destination/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:question_id>', views.question, name='question'),
 
-    path('<int:question_id>', views.result, name='result'),
-
-    path('<int:question_id>', views.vote, name='vote'),
 ]
